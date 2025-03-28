b5ec8bdecf9f180169b38ec4380c7ee2796c5c21
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2025-03-27 19:00:45 -0700

    Initial commit

---
+++ src/app/(pages)/_components/Projects/Projects.tsx
@@ -0,0 +1,9 @@
+// import styles from './About.module.scss';
+
+export default function Projects() {
+    return (
+    <div>
+        projects page
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+// import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <p>
+          main content goes here
+        </p>
+      </main>
+      <footer>
+        footer
+      </footer>
+    </div>
+  );
+}
