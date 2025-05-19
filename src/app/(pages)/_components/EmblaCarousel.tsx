54b2f08d3023937b45d5faaa9bc4485618c4eaf7
Haylie Tan <haylie.tan.code@gmail.com>
2025-05-19 09:42:39 -0700

    Removed unnecessary files

--- src/app/(pages)/_components/EmblaCarousel.tsx
+++ src/app/(pages)/_components/EmblaCarousel.tsx
@@ -42,7 +42,7 @@ export default function EmblaCarousel() {
       </div>
 
       {/* Vertical dot nav (optional on right edge) */}
-      <div className="absolute right-4 top-1/2 -translate-y-1/2 flex flex-col gap-2">
+      <div className="absolute left-4 top-3/4 -translate-y-1/2 flex flex-col gap-2">
         {scrollSnaps.map((_, index) => (
           <button
             key={index}
