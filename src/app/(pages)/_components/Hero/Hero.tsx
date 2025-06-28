bdb87dc4415abf5cda538dd22bd177b8c9296aac
Haylie Tan <haylie.tan.code@gmail.com>
2025-06-28 09:27:12 -0700

    created left and right panel for hero

--- src/app/(pages)/_components/Hero/Hero.tsx
+++ src/app/(pages)/_components/Hero/Hero.tsx
@@ -3,19 +3,30 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <div className={styles.introWrapper}>
-    <div className={styles.textBlock}>
-      <p className={styles.midTextLeft}>Hi! My name is</p>
-      <h1 className={styles.name}>Haylie Tan</h1>
-      <p className={styles.midTextRight}>
-        and I’m a
-      </p>
-      <p><span className={styles.role}>Full-Stack Developer.</span></p>
+    <div className={styles.container}>
+      <div className={styles.iconWrapper}>
+        <div className={styles.avatarBadge}>
+          <Image src="/Images/Ocean.jpg" alt="Ocean" width={240} height={340} />
+        </div>
+      </div>
+
+      <div className={styles.introWrapper}>
+        <div className={styles.textBlock}>
+          <p className={styles.midTextLeft}>Hi! My name is</p>
+          <h1 className={styles.name}>Haylie Tan</h1>
+          <p className={styles.midTextRight}>
+            and I’m a
+          </p>
+          <p className={styles.role}>Full-Stack Developer.</p>
+        </div>
+
+        <div className={styles.textRectangle}>
+          <p>I’m a collaborative full-stack developer passionate about making technical things human-friendly. I thrive in fast-paced teams where I can take ownership, debug under pressure, and build things people actually use.</p>
+        </div>
+      </div>
     </div>
-    <div className={styles.avatarBadge}>
-      <Image src="/avatar.jpg" alt="avatar" width={40} height={40} />
-    </div>
-  </div>
+
+
 
   );
 }
